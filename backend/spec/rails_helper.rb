fe1de5f556d9f208a86eac44f9da698d5c2949e0
Daniel C <2736635+bitclaw@users.noreply.github.com>
2026-03-23 17:52:43 -0500

    Add spec/support auto-loading and request helper
    - Enable the spec/support glob loader in rails_helper so shared
      helpers are picked up automatically.
    
    - Add RequestHelpers module with a json_body convenience method
      (parses response.body as symbolized JSON) and include it for
      all request specs.
    
    Commands run:
      # Verify RSpec still boots cleanly after the change
      docker-compose run --rm backend bundle exec rspec --dry-run

--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -26,7 +26,7 @@ require 'shoulda-matchers'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 # Ensures that the test database schema matches the current schema file.
 # If there are pending migrations it will invoke `db:test:prepare` to
@@ -46,6 +46,7 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include RequestHelpers, type: :request
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
